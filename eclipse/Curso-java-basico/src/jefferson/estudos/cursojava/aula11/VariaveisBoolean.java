a796d86884fc83e9942d21daba245c413982a5c2
Jefferson Rodrigues <psyjef@hotmail.com>
2016-05-31 10:27:31 -0300

    Continuação Curso Java
    Enviando continuação das Aulas do curso de java básico

---
+++ eclipse/Curso-java-basico/src/jefferson/estudos/cursojava/aula11/VariaveisBoolean.java
@@ -0,0 +1,15 @@
+package jefferson.estudos.cursojava.aula11;
+
+public class VariaveisBoolean {
+
+	public static void main(String[] args) {
+		
+		boolean verdadeiro = true;	
+		boolean falso = false;
+		
+		System.out.println("Valor de verdadeiro = " + verdadeiro );
+		System.out.println("Valor de falso = " + falso );
+
+	}
+
+}
---
+++ eclipse/Curso-java-basico/src/jefferson/estudos/cursojava/aula11/VariaveisPontoFlutuante.java
@@ -0,0 +1,16 @@
+package jefferson.estudos.cursojava.aula11;
+
+public class VariaveisPontoFlutuante {
+
+	public static void main(String[] args) {
+
+		//Declaração de Variáveis
+		double valorPassagem = 2.90;		
+		float valorTomate = 3.95f;
+		
+		System.out.println("Valor da Passagem = " + valorPassagem );		
+		System.out.println("Valor do tomate = " + valorTomate );
+
+	}
+
+}
